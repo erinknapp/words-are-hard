d3251b1848420ad311f622e883b70679219cb838
Erin Knapp <erin.knapp@gmail.com>
2022-02-02 18:47:32 -0500

    post api cant be found sadface

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -90,15 +90,15 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.put('/upvote', withAuth, (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.put('/upvote', withAuth, (req, res) => {
+//   // custom static method created in models/Post.js
+//   Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+//     .then(updatedVoteData => res.json(updatedVoteData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
