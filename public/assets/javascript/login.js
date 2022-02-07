dd0ae9835d16d4c2ac9d018a159b237302232361
Spoon God <spoon.is.god@gmail.com>
2022-02-07 17:42:37 -0500

    fixed main add code to feed and homepage

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -15,7 +15,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/feed/');
     } else {
       alert(response.statusText);
     }
@@ -41,7 +41,7 @@ async function signupFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/feed/');
     } else {
       alert(response.statusText);
     }
