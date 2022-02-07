ba1d6a8c9b27cbbad4fb89b66f2bddaffc497e2b
CBoensch <cboensch6505@gmail.com>
2022-02-06 19:48:09 -0500

    fixed the post btn errors

--- utils/giphy.js
+++ utils/giphy.js
@@ -1,16 +1,16 @@
-function sendApiRequest() {
-    var userInput = document.getElementById("input").value
-    console.log(userInput)
+// function sendApiRequest() {
+//     var userInput = document.getElementById("input").value
+//     console.log(userInput)
 
-    var giphyApiKey = "WnnzeT61NGgg3RtGQXPZDDr25T808hKc"
-    var giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}`
+//     var giphyApiKey = "WnnzeT61NGgg3RtGQXPZDDr25T808hKc"
+//     var giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}`
 
-    fetch(giphyApiURL).then(data => {
-        return data.json()
-    })
-    .then(giphyData => {
-        console.log(giphyData)
-    })
-}
+//     fetch(giphyApiURL).then(data => {
+//         return data.json()
+//     })
+//     .then(giphyData => {
+//         console.log(giphyData)
+//     })
+// }
 
-module.exports = sendApiRequest;
+// module.exports = sendApiRequest;
