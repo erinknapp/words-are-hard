a4197ca0832b86ffef78e13d048fc49a472bc5ac
Erin Knapp <erin.knapp@gmail.com>
2022-02-12 11:04:51 -0500

    fixed post routes in gify file

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -5,22 +5,22 @@
 // });
 
 // Gender Select
-if (window.location.pathname === "/") {
-  const radioBtn1 = document.querySelector("#flexRadioDefault1");
-  const radioBtn2 = document.querySelector("#flexRadioDefault2");
-  const radioBtn3 = document.querySelector("#flexRadioDefault3");
-  const genderSelect = document.querySelector("#genderSelect");
+// if (window.location.pathname === "/") {
+//   const radioBtn1 = document.querySelector("#flexRadioDefault1");
+//   const radioBtn2 = document.querySelector("#flexRadioDefault2");
+//   const radioBtn3 = document.querySelector("#flexRadioDefault3");
+//   const genderSelect = document.querySelector("#genderSelect");
 
-  radioBtn1.addEventListener("change", () => {
-    genderSelect.classList.add("d-none");
-  });
-  radioBtn2.addEventListener("change", () => {
-    genderSelect.classList.add("d-none");
-  });
-  radioBtn3.addEventListener("change", () => {
-    genderSelect.classList.remove("d-none");
-  });
-}
+//   radioBtn1.addEventListener("change", () => {
+//     genderSelect.classList.add("d-none");
+//   });
+//   radioBtn2.addEventListener("change", () => {
+//     genderSelect.classList.add("d-none");
+//   });
+//   radioBtn3.addEventListener("change", () => {
+//     genderSelect.classList.remove("d-none");
+//   });
+// }
 
 
 
